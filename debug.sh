f76d27c520c67754587b7b1ae0ff14fb12fd6d48
Neko Null <jerrylu.mail@qq.com>
2023-08-27 12:04:51 +0000

    fix: fix disqus partially

---
+++ debug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if "hugo" exists in the current directory
+if [ -x "$(pwd)/hugo" ]; then
+  echo "Executable 'hugo' found in the current directory."
+else
+  echo "Executable 'hugo' not found. Downloading and extracting..."
+  url="https://github.com/gohugoio/hugo/releases/download/v0.82.0/hugo_0.82.0_Linux-64bit.tar.gz"
+  wget -q "$url" -O hugo.tar.gz
+  tar -xzf hugo.tar.gz hugo
+  rm hugo.tar.gz
+  echo "Successfully extracted 'hugo'."
+fi
+
+./hugo serve
