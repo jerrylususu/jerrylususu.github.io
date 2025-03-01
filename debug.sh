ab900b872d00bb57cea5862034d27ad5484d4ab2
Neko Null <jerrylu.ms@outlook.com>
2025-03-02 00:42:16 +0800

    chore: bump hugo version

--- debug.sh
+++ debug.sh
@@ -5,7 +5,7 @@ if [ -x "$(pwd)/hugo" ]; then
   echo "Executable 'hugo' found in the current directory."
 else
   echo "Executable 'hugo' not found. Downloading and extracting..."
-  url="https://github.com/gohugoio/hugo/releases/download/v0.82.0/hugo_0.82.0_Linux-64bit.tar.gz"
+  url="https://github.com/gohugoio/hugo/releases/download/v0.145.0/hugo_0.145.0_Linux-64bit.tar.gz"
   wget -q "$url" -O hugo.tar.gz
   tar -xzf hugo.tar.gz hugo
   rm hugo.tar.gz
